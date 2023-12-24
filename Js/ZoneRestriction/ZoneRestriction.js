002bed8b4b1da43ebe239f04855ca41d34f3453f
KichDM <kichdmwa@gmail.com>
2023-12-24 12:14:18 +0100

    fix

--- Js/ZoneRestriction/ZoneRestriction.js
+++ Js/ZoneRestriction/ZoneRestriction.js
@@ -71,7 +71,6 @@ function On_Command(Player, cmd, args) {
 function On_EntityDeployed(Player, Entity) {
     var item = Entity.Name;
     var playerloc = Player.Location;
-
     if (Player != null && Entity != null) {
         var ini = initt();
         var tt = ini.Sections;
@@ -82,13 +81,13 @@ function On_EntityDeployed(Player, Entity) {
             var zonavector = Util.ConvertStringToVector3(zonaloc);
             var Dist = Util.GetVectorsDistance(playerloc, zonavector);
             var alejarse = parseInt(radio) - Dist;
-
             if (Dist <= parseInt(radio) && !zonaNotificada) {
                 Player.Message("[color#FF8000]You are in the area of [color#FF0040]" + seciones.toString() + "[color#FF8000]. You cannot build here. Move away [color#FF0040]" + alejarse.toFixed(0) + "[color#FF8000] meters.");
                 Entity.Destroy();
                 Player.InventoryNotice("+1 " + StringFix(item));
-                Player.Inventory.AddItem(StringFix(item), 1);
+                Player.Inventory.AddItem(StringFix(item));
                 zonaNotificada = true;
+                break; // Break out of the loop when the condition is met
             }
         }
     }
