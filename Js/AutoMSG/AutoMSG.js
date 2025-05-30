cff168af8d1dabb620b77d10ae5777a29403d697
KichDM <79092622+KichDM@users.noreply.github.com>
2025-05-30 17:54:04 +0200

    fix

--- Js/AutoMSG/AutoMSG.js
+++ Js/AutoMSG/AutoMSG.js
@@ -3,8 +3,8 @@ var About = "Announcement System"
 var Version = "1.0.0"
 
 // et in minutes, and it will perform a random calculation to choose a minute at random among them to create a timer.
-var mintime = 2;
-var maxtime = 10;
+var mintime = 1;
+var maxtime = 2;
 
 // The list of announcements, it's easy, you just need to put the announcements you want here, and the timer will randomly choose one from this list
 var Announcement = [
@@ -17,51 +17,31 @@ var Announcement = [
   "Test.",
   "Test.",
   "Test."
-  ];
+];
 
-  function On_ServerInit() {
-    var finaltime = NumeroRandom(mintime,maxtime);
-    var tiempo =  minutosAMilisegundos(Number(finaltime).toFixed(2));
-	Plugin.CreateTimer("Announcement", tiempo).Start();
-}
+function On_ServerInit() {
+  var finaltime = NumeroRandom(mintime, maxtime);
+  var tiempo = minutosAMilisegundos(Number(finaltime).toFixed(2));
+  var bucle = 0;
+  var activar = tiempo;
 
-  function AnuncioRandom() {
-    var random = Math.floor(Math.random() * Announcement.length);
-    var elaununciofinal = Announcement[random];
-    Server.Broadcast("[color#DAA520]" + elaununciofinal);
-  }
-
-  function NumeroRandom(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  function minutosAMilisegundos(minutos) {
-    var milisegundos = minutos * 60 * 1000;
-    return milisegundos;
-  }
-
-function AnnouncementCallback() {
+  Plugin.CreateParallelTimer("AutoAnuncios", activar, null, function (evt) {
     AnuncioRandom();
-    Plugin.KillTimer("Announcement");
-    var finaltime = NumeroRandom(mintime,maxtime);
-    var tiempo =  minutosAMilisegundos(Number(finaltime).toFixed(2));
-	Plugin.CreateTimer("Announcement", tiempo).Start();
+    bucle++;
+  }, 0);
 }
 
-//Only when this plugin reloads.
-function On_PluginInit() {
-    if (!Plugin.GetTimer("Announcement"))
-        {
-          var finaltime = NumeroRandom(mintime,maxtime);
-            var tiempo =  minutosAMilisegundos(Number(finaltime).toFixed(2));
-            Server.Broadcast("Numero Random " + finaltime);
-            Server.Broadcast("Tiempo " + tiempo);
-            Plugin.CreateTimer("Announcement", tiempo).Start();
-        }
-        else {
-            Plugin.KillTimer("Announcement");
-            var finaltime = NumeroRandom(mintime,maxtime);
-            var tiempo =  minutosAMilisegundos(Number(finaltime).toFixed(2));
-            Plugin.CreateTimer("Announcement", tiempo).Start();
-        }
-  }
+function AnuncioRandom() {
+  var random = Math.floor(Math.random() * Announcement.length);
+  var elaununciofinal = Announcement[random];
+  Server.Broadcast("[color#DAA520]" + elaununciofinal);
+}
+
+function NumeroRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function minutosAMilisegundos(minutos) {
+  var milisegundos = minutos * 60 * 1000;
+  return milisegundos;
+}
